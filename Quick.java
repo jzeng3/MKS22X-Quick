ff4e43c1fb3068263742d69248d2a36f8302a0ba
Jamie Zeng <jzeng3@stuy.edu>
2019-03-14 21:21:20 -0400

    updated comments :)

--- Quick.java
+++ Quick.java
@@ -3,68 +3,67 @@ import java.io.*;
 
 public class Quick{
   public static void main(String[]args){
-  System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
-  int[]MAX_LIST = {1000000000,500,10};
-  for(int MAX : MAX_LIST){
-    for(int size = 31250; size < 2000001; size*=2){
-      long qtime=0;
-      long btime=0;
-      //average of 5 sorts.
-      for(int trial = 0 ; trial <=5; trial++){
-        int []data1 = new int[size];
-        int []data2 = new int[size];
-        for(int i = 0; i < data1.length; i++){
-          data1[i] = (int)(Math.random()*MAX);
-          data2[i] = data1[i];
+    System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
+    int[]MAX_LIST = {1000000000,500,10};
+    for(int MAX : MAX_LIST){
+      for(int size = 31250; size < 2000001; size*=2){
+        long qtime=0;
+        long btime=0;
+        //average of 5 sorts.
+        for(int trial = 0 ; trial <=5; trial++){
+          int []data1 = new int[size];
+          int []data2 = new int[size];
+          for(int i = 0; i < data1.length; i++){
+            data1[i] = (int)(Math.random()*MAX);
+            data2[i] = data1[i];
+          }
+          long t1,t2;
+          t1 = System.currentTimeMillis();
+          Quick.quicksort(data2);
+          t2 = System.currentTimeMillis();
+          qtime += t2 - t1;
+          t1 = System.currentTimeMillis();
+          Arrays.sort(data1);
+          t2 = System.currentTimeMillis();
+          btime+= t2 - t1;
+          if(!Arrays.equals(data1,data2)){
+            System.out.println("FAIL TO SORT!");
+            System.exit(0);
+          }
         }
-        long t1,t2;
-        t1 = System.currentTimeMillis();
-        Quick.quicksort(data2);
-        t2 = System.currentTimeMillis();
-        qtime += t2 - t1;
-        t1 = System.currentTimeMillis();
-        Arrays.sort(data1);
-        t2 = System.currentTimeMillis();
-        btime+= t2 - t1;
-        if(!Arrays.equals(data1,data2)){
-          System.out.println("FAIL TO SORT!");
-          System.exit(0);
-        }
-      }
-      System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
+        System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
 
+      }
+      System.out.println();
     }
-    System.out.println();
   }
-}
 
   /*Modify the array to be in increasing order.
-   */
-   public static void quicksort(int[] data){
-     quicksortH(data,0,data.length-1);
-   }
+  */
+  public static void quicksort(int[] data){
+    quicksortH(data,0,data.length-1); // call helper function
+  }
 
-   // quicksort helper takes lo, hi
-   public static void quicksortH(int[] data, int lo, int hi){
-     if (lo >= hi){
-       return; // end function if lo is greater than high
-     }
-     // otherwise record the pivot index
-     int pivot = partitionDutch(data,lo,hi);
-     // recursively call quicksortH on left and right sides of array
-     quicksortH(data, lo, pivot -1);
-     quicksortH(data,pivot+1,hi);
-   }
+  // quicksort helper takes lo, hi indices
+  public static void quicksortH(int[] data, int lo, int hi){
+    if (lo >= hi){
+      return; // end function if lo is greater than high
+    }
+    // otherwise record the pivot index
+    int pivot = partitionDutch(data,lo,hi);
+    // recursively call quicksortH on left and right sides of array
+    quicksortH(data, lo, pivot -1);
+    quicksortH(data,pivot+1,hi);
+  }
 
-/*  a - When choosing a pivot, use the median value of the lo,hi, and middle elements.
-    b - When a data element is equal to the pivot, make a 50% chance that you swap it to the other
-    */
+  /*  a - When choosing a pivot, use the median value of the lo,hi, and middle elements.
+  b - When a data element is equal to the pivot, make a 50% chance that you swap it to the other
+  */
   public static int partitionDutch(int[] data,int lo, int hi){
     if (lo == hi){
       return lo; // if there's only one element, return the element
     }
-
-    int mid = (lo + hi) / 2;
+    // if the section is only two elements, sort the two elements and return the lower index
     if (hi - lo == 1){
       if (data[lo] > data[hi]){
         int temp4 = data[hi];
@@ -74,7 +73,12 @@ public class Quick{
       return lo;
     }
 
-    int pivot = 0;
+    int mid = (lo + hi) / 2; // middle index
+    // s and e keep track of the low and high indices
+    int s = lo;
+    int e = hi;
+
+    int pivot = 0; // pivot index
     // set pivot as the median value of lo,hi,mid elements
     if (data[lo] > data[hi] && data[lo] < data[mid]
     ||  data[lo] < data[hi] && data[lo] > data[mid]){
@@ -88,31 +92,41 @@ public class Quick{
       pivot = hi;
     }
 
-    int s = lo;
-    int e = hi;
-
     // move pivot to the beginning of the array part that's being sorted
-      int temp = data[lo];
-      data[lo] = data[pivot];
-      data[pivot] = temp;
-      s++; // update loing index
-      Random rand = new Random();
+    int temp = data[lo];
+    data[lo] = data[pivot];
+    data[pivot] = temp;
+    s++; // update start index
+
+    /* Random for deciding whether equal value of pivot element
+    should be moved to the other side of the array*/
+    Random rand = new Random();
+
+    // while start is less than end
     while(s < e){
+      // willChange is either 0 or 1: 0 for moving equal value to other side and 1 for not
       int willChange = rand.nextInt(2);
-      // if current element is greater than pivot element, move element to the right hi
+
+      /* if current element is greater than pivot element or
+      if current element is equal to pivot and willChange equals 0
+      switch lo and hi elements and update the hi index */
+
       if (data[s] > data[lo] ||
-          (data[s] == data[lo] && willChange == 0)){
+      (data[s] == data[lo] && willChange == 0)){
         int temp1 = data[e];
         data[e] = data[s];
         data[s] = temp1;
         e--;
       }
-      // if current element is less than pivot element, move the lo to the right
+      /* if current element is less than pivot element or
+      if current element is equal to pivot and willChange equals 1
+      update the lo element index */
       else if (data[s] < data[lo] ||
-               (data[s] == data[lo] && willChange == 1)){
+      (data[s] == data[lo] && willChange == 1)){
         s++;
       }
     }
+
     // find where the pivot element belongs, place it in that position, and return the index
     int index = 0;
     if (data[lo] <= data[s]){
@@ -124,12 +138,13 @@ public class Quick{
     int temp3 = data[index];
     data[index] = data[lo];
     data[lo] = temp3;
-    return index;
-}
 
-/*return the value that is the kth smallest value of the array.
-*/
-public static int quickselect(int[] data, int k){
+    return index;
+  }
+
+  /*return the value that is the kth smallest value of the array.
+  */
+  public static int quickselect(int[] data, int k){
     int startIndex = 0;
     int endIndex = data.length - 1;
     int index = partitionDutch(data, startIndex, endIndex);
@@ -147,9 +162,9 @@ public static int quickselect(int[] data, int k){
         index = partitionDutch(data, startIndex, endIndex);
       }
     }
-  return data[index];
+    return data[index];
 
-}
+  }
 
   /*Choose a random pivot element between the start and end index inclusive,
   Then modify the array such that:
@@ -173,20 +188,20 @@ public static int quickselect(int[] data, int k){
     }
 
     // move pivot to the beginning of the array part that's being sorted
-      int temp = data[start];
-      data[start] = data[pivot];
-      data[pivot] = temp;
-      s++; // update starting index
+    int temp = data[start];
+    data[start] = data[pivot];
+    data[pivot] = temp;
+    s++; // update starting index
 
     while(s < e){
-      // if current element is greater than pivot element, move element to the right end
+      // if current element is greater than pivot element, swap start and end elements and update end index
       if (data[s] > data[start]){
         int temp1 = data[e];
         data[e] = data[s];
         data[s] = temp1;
         e--;
       }
-      // if current element is less than pivot element, move the start to the right
+      // if current element is less than or equal to the pivot element, move the start index to the right
       else if (data[s] <= data[start]){
         s++;
       }
